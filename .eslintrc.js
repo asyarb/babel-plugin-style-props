03dfab812834c10e52375097bd13e38d1c6f8d0c
git <asyarb@hawaii.edu>
2019-08-25 23:26:06 -1000

    feat; packages for emotion and sc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
+  plugins: ['react'],
   rules: {
     'no-var': 'warn',
     'no-unused-vars': 'warn',
     'no-undef': 'error',
+
+    // react plugin - options
+    'react/jsx-uses-react': 'warn',
+    'react/jsx-uses-vars': 'warn',
+    'react/jsx-key': 'warn',
   },
   env: {
     node: true,
     browser: true,
     jest: true,
-    es6: true,
   },
   parser: 'babel-eslint',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 8,
   },
 }
---
+++ packages/styled-components/__tests__/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import 'jest-styled-components'
+import '@testing-library/jest-dom/extend-expect'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../theme'
+
+const Providers = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+)
+
+// react-testing-library's render function with wrapped providers and addition return values.
+const customRender = Comp => {
+  const { container, ...rest } = render(Comp, { wrapper: Providers })
+  const key = Object.keys(container.firstChild)[0]
+  const FiberNode = container.firstChild[key]
+  const props = FiberNode.pendingProps
+
+  return { container, props, result: container.firstChild, ...rest }
+}
+
+describe('styled-components', () => {
+  it('actually works', () => {
+    const { result, props } = customRender(<div color="tomato" bg="#fff" />)
+  })
+})
