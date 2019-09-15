ec63dfb478f92fc8a914a91690a2265502f7a6fa
git <asyarb@hawaii.edu>
2019-09-14 18:26:49 -1000

    feat: emotion example

--- packages/emotion/index.js
+++ packages/emotion/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const Hello = ({ ...props }) => {
-  return <div myScale={['l', null, 'm']} {...props} />
+  return <div myScale={['l', null, 'm']} p={[0, null, 3]} {...props} />
 }
