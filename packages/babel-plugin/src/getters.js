b8d0bc6a6e9efe2c9fbf0e724abdc477aa13707e
Anthony <asyarb@hawaii.edu>
2019-09-19 19:20:46 -1000

    feat: conditionally get dynamic scale values

--- packages/babel-plugin/src/getters.js
+++ packages/babel-plugin/src/getters.js
@@ -1,5 +1,5 @@
 export const getStaticValue = (theme, key, value) => {
-  if (theme[key][value] !== undefined) return theme[key][value]
+  if (theme[key] && theme[key][value] !== undefined) return theme[key][value]
 
   return value
 }
@@ -13,10 +13,13 @@ export const getDynamicValue = (
 ) => {
   if (value === undefined) return null
 
-  if (theme[scaleKey][value] !== undefined)
-    return theme[scaleKey][value][mediaIndex]
+  if (theme[scaleKey] && theme[scaleKey][value] !== undefined)
+    return theme[scaleKey][value][
+      Math.min(mediaIndex, theme[scaleKey][value].length)
+    ]
 
-  if (theme[baseKey][value] !== undefined) return theme[baseKey][value]
+  if (theme[baseKey] && theme[baseKey][value] !== undefined)
+    return theme[baseKey][value]
 
   return value
 }
